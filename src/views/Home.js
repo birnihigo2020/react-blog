74bcf7b24ed72a9ebf5d8b0927bc63a5afbbc33c
Solomon <solomon@Solomons-MBP.attlocal.net>
2021-08-08 16:23:49 -0500

    creating Home and BlogNavbar components

---
+++ src/views/Home.js
@@ -0,0 +1,50 @@
+/*!
+
+=========================================================
+* Argon Design System React - v1.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// reactstrap components
+import { Container, Row } from "reactstrap";
+
+// core components
+import BlogNavbar from "components/Navbars/BlogNavbar.js";
+import CardsFooter from "components/Footers/CardsFooter.js";
+
+class Home extends React.Component {
+  componentDidMount() {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    this.refs.main.scrollTop = 0;
+  }
+  render() {
+    return (
+      <>
+        <BlogNavbar />
+        <main ref="main">
+            <Container>
+                <Row>
+                    <p> This is the main content </p>
+                </Row>
+            </Container>
+        </main>
+        <CardsFooter />
+      </>
+    );
+  }
+}
+
+export default Home;
